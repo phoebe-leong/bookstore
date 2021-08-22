48944caf3ebce9e9809015c8456fbb028d22fef5
ph3b3 <phoebeleong8@gmail.com>
2021-08-22 10:51:49 +1000

    updated identifier and budget types

--- customer.cpp
+++ customer.cpp
@@ -11,8 +11,8 @@ struct bookinfo {
 sqlite3* db;
 sqlite3_stmt* statement;
 std::vector<bookinfo> bought;
-size_t identifier;
-size_t budget; /* Just in case Bill Gates wants to set his bank account to be his budget */
+uint_max_t identifier;
+uint_max_t budget; /* Just in case Bill Gates wants to set his bank account to be his budget */
 int menu();
 
 void signup();
